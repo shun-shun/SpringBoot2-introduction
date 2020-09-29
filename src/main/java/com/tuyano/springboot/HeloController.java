374e0a4f2fad9faf9b36db9f86d843afa4527f06
権藤　俊 <gondo.syun0711@gmail.com>
2020-09-29 21:27:44 +0900

    add form tags.

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -2,20 +2,22 @@ package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HeloController {
 
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("msg", "お名前を書いて送信してください。");
+		return "index";
+	}
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		int res = 0;
-		for(int i = 1; i <= num; i++) {
-			res += i;
-		}
-		model.addAttribute("msg", "total: " + res);
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String send(@RequestParam("text1") String str, Model model) {
+		model.addAttribute("msg", "こんにちは、 " + str + "さん！");
 		return "index";
 	}
 }
