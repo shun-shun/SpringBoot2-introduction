643f480c789575ce7d25e7f41e743727acb6afca
権藤　俊 <gondo.syun0711@gmail.com>
2020-09-29 21:14:21 +0900

    add dependency for Thymeleaf.

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,58 +1,14 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HeloController {
 
-	String[] names = {
-			"tuyano","hanako","taro","sachiko","ichiro"
-	};
-	String[] mails = {
-			"shoda@tuuyano.com","hanako@flower","taro@yamada","sachiko@happy","ichiro@baseball"
-	};
 
-	@RequestMapping("/{num}")
-	public DataObject index(@PathVariable int id) {
-		return new DataObject(id,names[id],mails[id]);
-	}
-}
-
-class DataObject {
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
 }
